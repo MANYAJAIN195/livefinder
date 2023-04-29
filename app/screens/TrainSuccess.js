1eeae797f99f5082d61a85cb64a8201fd6b0c2e0
kuk-84 <sakshikukal25@gmail.com>
2023-04-29 21:33:53 +0530

    train number

--- app/screens/TrainSuccess.js
+++ app/screens/TrainSuccess.js
@@ -6,11 +6,52 @@ import Screen from '../components/Screen'
 import Constants from 'expo-constants'
 import { Icon } from 'react-native-elements'
 import tailwind from 'tailwind-react-native-classnames'
+import { useState,useEffect } from 'react'
+import {RAILWAY_APIKEY, RAILWAY_HOST_APIKEY} from "@env";
+
+
 
 const TrainSuccess = ({ route }) => {
     const navigation = useNavigation()
     const {data} = route.params;
     const trainno=data.trainNo;
+    const [selectedTrainData, setSelectedTrainData] = useState([]);
+
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+          console.log('This will run after 3 second!')
+          liveStatus()
+        }, 3000);
+        return () => clearTimeout(timer);
+      }, []);
+
+
+    const liveStatus = async () => {
+    const URL = `https://irctc1.p.rapidapi.com/api/v1/liveTrainStatus?trainNo=${trainno}&startDay=1`;
+    const options = {
+        method: 'GET',
+        headers: {
+        'X-RapidAPI-Key': RAILWAY_APIKEY,
+        'X-RapidAPI-Host': RAILWAY_HOST_APIKEY,
+        },
+    };
+    
+    try {
+        const response = await fetch(URL, options);
+        const selectedTrain = await response.json();
+        const selTrainData = selectedTrain.data;
+        setSelectedTrainData(selTrainData);
+        console.log(selTrainData);
+        
+    } catch (error) {
+        console.error(error);
+    }
+    };
+    
+    
+
+    
 
     return (
         <Screen style={tw`bg-white h-full justify-center`}>
